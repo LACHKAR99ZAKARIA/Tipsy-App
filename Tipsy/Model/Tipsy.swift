51cf8328e881ae45ada13fe3da0a25339be74514
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-03-16 16:25:00 +0100

    first commit

---
+++ Tipsy/Model/Tipsy.swift
@@ -0,0 +1,9 @@
+//
+//  Tipsy.swift
+//  Tipsy
+//
+//  Created by zakaria lachkar on 3/8/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Tipsy/Model/Trips.swift
@@ -0,0 +1,9 @@
+//
+//  Trips.swift
+//  Tipsy
+//
+//  Created by zakaria lachkar on 3/8/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
